1d0084355161bc3530296a6cf5f0f4856f29605c
LooKeR <mohit2002ss@gmail.com>
2024-04-24 14:14:01 +0530

    Remove unused fonts

--- app/src/main/kotlin/com/looker/kenko/ui/theme/Type.kt
+++ app/src/main/kotlin/com/looker/kenko/ui/theme/Type.kt
@@ -9,114 +9,118 @@ import androidx.compose.ui.unit.sp
 import com.looker.kenko.R
 
 val FontFamily.Companion.Numbers
-    get() = FontFamily(Font(R.font.space_mono_bold))
+    get() = FontFamily(Font(R.font.spacemono_bold))
 
-val FontFamily.Companion.Oswald
+val FontFamily.Companion.SpaceMono
     get() = FontFamily(
-        Font(R.font.oswald_bold, weight = FontWeight.Bold),
-        Font(R.font.oswald_semibold, weight = FontWeight.SemiBold),
-        Font(R.font.oswald_medium, weight = FontWeight.Medium),
-        Font(R.font.oswald_normal, weight = FontWeight.Normal),
-        Font(R.font.oswald_light, weight = FontWeight.Light),
-    )
-
-val FontFamily.Companion.Poppins
-    get() = FontFamily(
-        Font(R.font.poppins_regular, weight = FontWeight.Normal),
-        Font(R.font.poppins_medium, weight = FontWeight.Medium),
-        Font(R.font.poppins_semibold, weight = FontWeight.SemiBold),
+        Font(R.font.spacemono_bold, weight = FontWeight.Bold),
+        Font(R.font.spacemono_normal, weight = FontWeight.Normal),
     )
 
 fun TextStyle.numbers() = copy(fontFamily = FontFamily.Numbers)
 
 val Typography = Typography(
     displayLarge = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.Normal,
-        fontSize = 72.sp,
-        lineHeight = 75.0.sp,
-        letterSpacing = (-1.4).sp
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 57.sp,
+        lineHeight = 64.0.sp,
+        letterSpacing = (-0.25).sp
     ),
     displayMedium = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Bold,
         fontSize = 45.sp,
         lineHeight = 52.0.sp,
         letterSpacing = 0.0.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Bold,
         fontSize = 36.sp,
         lineHeight = 44.0.sp,
         letterSpacing = 0.0.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.Normal,
-        fontSize = 38.sp,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
         lineHeight = 36.0.sp,
         letterSpacing = 0.0.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = FontFamily.Oswald,
+        fontFamily = FontFamily.SpaceMono,
         fontWeight = FontWeight.Bold,
         fontSize = 28.sp,
         lineHeight = 36.0.sp,
         letterSpacing = 0.0.sp
     ),
+    headlineSmall = TextStyle(
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
+        fontSize = 24.sp,
+        lineHeight = 32.0.sp,
+        letterSpacing = 0.0.sp
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.2.sp
     ),
     titleSmall = TextStyle(
-        fontFamily = FontFamily.Oswald,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Poppins,
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Poppins,
+        fontFamily = FontFamily.SpaceMono,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.2.sp
     ),
+    bodySmall = TextStyle(
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.4.sp
+    ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Poppins,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Poppins,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Poppins,
-        fontWeight = FontWeight.SemiBold,
+        fontFamily = FontFamily.SpaceMono,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
