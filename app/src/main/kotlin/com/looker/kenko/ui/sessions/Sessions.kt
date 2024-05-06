7a690666ecac6686cb81cae2db851199e97f9408
LooKeR <mohit2002ss@gmail.com>
2024-05-06 12:49:44 +0530

    Overhaul of Session Screen - Add ".today" label - Replace icon with texture - Cleaner cards

--- app/src/main/kotlin/com/looker/kenko/ui/sessions/Sessions.kt
+++ app/src/main/kotlin/com/looker/kenko/ui/sessions/Sessions.kt
@@ -1,18 +1,18 @@
 package com.looker.kenko.ui.sessions
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.FabPosition
@@ -25,15 +25,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
+import com.looker.kenko.R
 import com.looker.kenko.data.model.Session
+import com.looker.kenko.ui.components.texture.GradientStart
+import com.looker.kenko.ui.components.texture.dottedGradient
+import com.looker.kenko.ui.planEdit.components.dayName
 import com.looker.kenko.ui.theme.KenkoIcons
 import com.looker.kenko.ui.theme.KenkoTheme
 import com.looker.kenko.utils.DateTimeFormat
 import com.looker.kenko.utils.formatDate
+import com.looker.kenko.utils.isToday
 import kotlinx.datetime.LocalDate
 
 @Composable
@@ -53,7 +59,12 @@ fun Sessions(
                 onClick = { onSessionClick(null) },
                 contentPadding = PaddingValues(vertical = 32.dp, horizontal = 48.dp)
             ) {
-                Text(text = "Start")
+                val isActive by viewModel.isCurrentSessionActive.collectAsState()
+                val text = remember(isActive) {
+                    if (isActive) R.string.label_continue
+                    else R.string.label_start
+                }
+                Text(text = stringResource(id = text))
                 Spacer(modifier = Modifier.width(16.dp))
                 Icon(
                     imageVector = KenkoIcons.ArrowForward,
@@ -68,18 +79,22 @@ fun Sessions(
         LazyColumn(
             modifier = Modifier.fillMaxSize(),
             contentPadding = padding,
-            verticalArrangement = Arrangement.spacedBy(6.dp)
+            verticalArrangement = Arrangement.spacedBy(2.dp)
         ) {
             items(
                 items = sessions,
                 key = { it.date.toEpochDays() },
-                contentType = { it.date.dayOfWeek }
             ) { session ->
                 SessionCard(
                     session = session,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 12.dp),
+                    isTodayLabel = {
+                        val isToday = remember(session.date) {
+                            session.date.isToday
+                        }
+                        if (isToday) {
+                            IsTodayLabel()
+                        }
+                    },
                     onClick = { onSessionClick(session.date) }
                 )
             }
@@ -88,34 +103,52 @@ fun Sessions(
 }
 
 @Composable
+private fun IsTodayLabel() {
+    Surface(
+        shape = CircleShape,
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.primary),
+        color = MaterialTheme.colorScheme.secondaryContainer,
+    ) {
+        Text(
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            text = stringResource(R.string.label_today),
+            style = MaterialTheme.typography.labelLarge,
+            color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+    }
+}
+
+@Composable
 fun SessionCard(
     session: Session,
     modifier: Modifier = Modifier,
+    isTodayLabel: @Composable () -> Unit,
     onClick: () -> Unit = {},
 ) {
     Surface(
         modifier = modifier,
-        shape = MaterialTheme.shapes.large,
         color = MaterialTheme.colorScheme.surfaceContainerLowest,
         onClick = onClick
     ) {
-        Row(
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .padding(horizontal = 18.dp, vertical = 28.dp),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Column {
-                Text(
-                    text = session.formattedDate(),
-                    style = MaterialTheme.typography.titleLarge
+                .dottedGradient(
+                    MaterialTheme.colorScheme.onSurfaceVariant,
+                    start = GradientStart.TopRight
                 )
-            }
-            SessionIcon(
-                dayOfWeek = session.date.dayOfWeek,
-                modifier = Modifier.size(72.dp)
+                .padding(horizontal = 18.dp, vertical = 28.dp),
+        ) {
+            isTodayLabel()
+            Text(
+                text = dayName(session.date.dayOfWeek),
+                style = MaterialTheme.typography.headlineMedium,
+            )
+            Text(
+                text = session.formattedDate(),
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.outline
             )
         }
     }
@@ -136,10 +169,19 @@ fun Session.formattedDate(format: DateTimeFormat = DateTimeFormat.Short): String
 
 @Preview
 @Composable
+private fun TodayLabelPreview() {
+    KenkoTheme {
+        IsTodayLabel()
+    }
+}
+
+@PreviewLightDark
+@Composable
 private fun SessionCardPreview() {
     KenkoTheme {
         SessionCard(
             session = Session.SAMPLE,
+            isTodayLabel = { IsTodayLabel() },
             modifier = Modifier.fillMaxWidth()
         )
     }
--- app/src/main/kotlin/com/looker/kenko/ui/sessions/SessionsViewModel.kt
+++ app/src/main/kotlin/com/looker/kenko/ui/sessions/SessionsViewModel.kt
@@ -1,14 +1,23 @@
 package com.looker.kenko.ui.sessions
 
 import androidx.lifecycle.ViewModel
+import com.looker.kenko.data.model.localDate
 import com.looker.kenko.data.repository.SessionRepo
 import com.looker.kenko.utils.asStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 @HiltViewModel
 class SessionsViewModel @Inject constructor(
     repo: SessionRepo,
 ) : ViewModel() {
-    val sessionsStream = repo.stream.asStateFlow(emptyList())
+    val sessionsStream = repo.stream.map { it.asReversed() }.asStateFlow(emptyList())
+
+    val isCurrentSessionActive: StateFlow<Boolean> =
+        repo.getStream(localDate)
+            .map { it != null }
+            .asStateFlow(false)
+
 }
