5b2ab881c7e982f3cdcb5a2018d06287fa231c01
LooKeR <mohit2002ss@gmail.com>
2024-09-04 15:24:12 +0530

    Fix migration issue with nullable primary key

--- app/src/main/kotlin/com/looker/kenko/data/local/Migrations.kt
+++ app/src/main/kotlin/com/looker/kenko/data/local/Migrations.kt
@@ -5,42 +5,55 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(db: SupportSQLiteDatabase) {
-        db.execSQL("ALTER TABLE `plan_table` RENAME TO `plans`")
+        db.execSQL(
+            """
+            CREATE TABLE plans (
+            `name` VARCHAR(100) NOT NULL,
+            `exercisesPerDay` TEXT NOT NULL,
+            `isActive` INTEGER NOT NULL,
+            `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)
+            """.trimIndent()
+        )
+        db.execSQL(
+            """
+            INSERT INTO `plans` (`name`,`exercisesPerDay`, `isActive`) 
+            SELECT `name`,`exercisesPerDay`, `isActive` FROM `plan_table`
+            """.trimIndent()
+        )
         db.execSQL(
             """
             CREATE TABLE exercises (
-            `name` TEXT NOT NULL,
-            `target` TEXT NOT NULL,
+            `name` VARCHAR(100) NOT NULL,
+            `target` VARCHAR(15) NOT NULL,
             `reference` TEXT,
             `isIsometric` INTEGER NOT NULL,
-            `id` INTEGER PRIMARY KEY AUTOINCREMENT
-            )
-        """.trimIndent()
+            `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)
+            """.trimIndent()
         )
         db.execSQL(
             """
             INSERT INTO `exercises` (`name`,`target`,`reference`,`isIsometric`) 
             SELECT `name`,`target`,`reference`,`isIsometric` FROM `Exercise`
-        """.trimIndent()
+            """.trimIndent()
         )
         db.execSQL(
             """
             CREATE TABLE IF NOT EXISTS sessions (
             `date` INTEGER NOT NULL,
-            `sets` TEXT NOT NULL,
             `planId` INTEGER NOT NULL CONSTRAINT `fk_sessions_plans_id` REFERENCES `plans` (`id`) ON UPDATE NO ACTION ON DELETE CASCADE,
-            `id` INTEGER PRIMARY KEY AUTOINCREMENT
-            )
-        """.trimIndent()
+            `sets` TEXT NOT NULL,
+            `id` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT)
+            """.trimIndent()
         )
         db.execSQL(
             """
             INSERT INTO `sessions` (`date`,`sets`, `planId`) 
             SELECT Session.`date`, Session.`sets`, plans.`id`
             FROM `Session` INNER JOIN `plans` ON plans.`isActive` = 1 OR plans.`isActive` = 0
-        """.trimIndent()
+            """.trimIndent()
         )
         db.execSQL("DROP TABLE `Session`")
         db.execSQL("DROP TABLE `Exercise`")
+        db.execSQL("DROP TABLE `plan_table`")
     }
 }
