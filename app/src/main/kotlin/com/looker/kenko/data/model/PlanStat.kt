d6e9c5f0bcc609debc34c83ed56134156ec89aa8
LooKeR <mohit2002ss@gmail.com>
2025-03-30 00:59:45 +0530

    feat: Use Compose utils to pack and unpack values
    This change replaces custom packing and unpacking logic with Compose's `packInts`, `unpackInt1`, and `unpackInt2` utils for improved efficiency and consistency.

--- app/src/main/kotlin/com/looker/kenko/data/model/PlanStat.kt
+++ app/src/main/kotlin/com/looker/kenko/data/model/PlanStat.kt
@@ -1,9 +1,24 @@
-@file:Suppress("NOTHING_TO_INLINE")
+/*
+ * Copyright (C) 2025 LooKeR & Contributors
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 package com.looker.kenko.data.model
 
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
+import androidx.compose.ui.util.packInts
+import androidx.compose.ui.util.unpackInt1
+import androidx.compose.ui.util.unpackInt2
 
 @Immutable
 @JvmInline
@@ -23,14 +38,3 @@ fun PlanStat(exercises: Int, workDays: Int): PlanStat {
     return PlanStat(packInts(exercises, workDays))
 }
 
-private inline fun packInts(a: Int, b: Int): Long {
-    return (a.toLong() shl 32) or (b.toLong() and 0xFFFFFFFF)
-}
-
-private inline fun unpackInt1(value: Long): Int {
-    return (value shr 32).toInt()
-}
-
-private inline fun unpackInt2(value: Long): Int {
-    return (value and 0xFFFFFFFF).toInt()
-}
