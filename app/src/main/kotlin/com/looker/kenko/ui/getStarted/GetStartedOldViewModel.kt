6d7c228c7c02f1015f263df0624dc8f8a0974c7c
LooKeR <mohit2002ss@gmail.com>
2025-05-17 02:08:33 +0530

    test copyright formatting
    Signed-off-by: LooKeR <mohit2002ss@gmail.com>

--- app/src/main/kotlin/com/looker/kenko/ui/getStarted/GetStartedOldViewModel.kt
+++ app/src/main/kotlin/com/looker/kenko/ui/getStarted/GetStartedOldViewModel.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2025. LooKeR & Contributors
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.looker.kenko.ui.getStarted
 
 import androidx.lifecycle.SavedStateHandle
@@ -11,7 +25,6 @@ import javax.inject.Inject
 class GetStartedOldViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
 ) : ViewModel() {
-
     private val routeData: GetStartedRoute = savedStateHandle.toRoute()
     val isOnboardingDone = routeData.isOnboardingDone
 
