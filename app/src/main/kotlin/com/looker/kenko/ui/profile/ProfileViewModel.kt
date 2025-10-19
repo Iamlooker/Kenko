a591ccd7dbc750cdfbe66f5239bef19c4c01b1fc
LooKeR <iamlooker@proton.me>
2025-10-20 00:55:38 +0530

    refactor: Make simpler state flow initialization in profile viewmodel
    Signed-off-by: LooKeR <iamlooker@proton.me>

--- app/src/main/kotlin/com/looker/kenko/ui/profile/ProfileViewModel.kt
+++ app/src/main/kotlin/com/looker/kenko/ui/profile/ProfileViewModel.kt
@@ -1,3 +1,17 @@
+/*
+ * Copyright (C) 2025. LooKeR & Contributors
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.looker.kenko.ui.profile
 
 import androidx.compose.runtime.Stable
@@ -9,10 +23,10 @@ import com.looker.kenko.data.repository.PlanRepo
 import com.looker.kenko.data.repository.SessionRepo
 import com.looker.kenko.utils.asStateFlow
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
-import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel @Inject constructor(
@@ -35,23 +49,14 @@ class ProfileViewModel @Inject constructor(
             planName = plan?.name ?: "",
             planStat = plan?.stat,
         )
-    }
-        .asStateFlow(
-            ProfileUiState(
-                numberOfExercises = 0,
-                isPlanAvailable = false,
-                planName = "",
-                totalLifts = 0,
-                planStat = null,
-            ),
-        )
+    }.asStateFlow(ProfileUiState())
 }
 
 @Stable
 data class ProfileUiState(
-    val numberOfExercises: Int,
-    val isPlanAvailable: Boolean,
-    val planName: String,
-    val totalLifts: Int,
-    val planStat: PlanStat?,
+    val numberOfExercises: Int = 0,
+    val isPlanAvailable: Boolean = false,
+    val planName: String = "",
+    val totalLifts: Int = 0,
+    val planStat: PlanStat? = null,
 )
