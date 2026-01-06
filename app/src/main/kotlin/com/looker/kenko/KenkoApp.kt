c6d2d8f2e08089725d5eb4f00a78103cb72647a4
LooKeR <iamlooker@proton.me>
2026-01-07 01:10:38 +0530

    feat: Add initializer for work manager

--- app/src/main/kotlin/com/looker/kenko/KenkoApp.kt
+++ app/src/main/kotlin/com/looker/kenko/KenkoApp.kt
@@ -15,7 +15,19 @@
 package com.looker.kenko
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class KenkoApp : Application()
+class KenkoApp : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
+}
